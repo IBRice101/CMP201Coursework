2144bc0fe57c0125c606f576b443ab2cf81e1ec1
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-11-20 02:02:39 +0000

    Created Random Genome Sequence Generator, with random length generator option

--- Strings/GenomeGen.py
+++ Strings/GenomeGen.py
@@ -1,15 +1,25 @@
 # A program to generate a random genome sequence (containing G, A, T, C), and output to txt file in this directory
 
 import random
+from random import seed
+from random import randint
 
 nucleobases = 'ACGT'
 
-num = int(input("How many nucleobases would you like to generate?"))
+rand = bool(input("Would you like the number of nucleobases to be random?"))
+
+seed(1)
+
+if rand:
+    maximum = int(input("What would you like the max value to be?"))
+    num = randint(0, maximum)
+else:
+    num = int(input("How many nucleobases would you like to generate?"))
 
 genome = ''.join((random.choice(nucleobases) for i in range(num)))
 
-f = open("genome" + str(num) + ".txt", "w")
+f = open("Genomes/Genome" + str(num) + ".txt", "w")
 f.write(str(genome))
 f.close()
 
-print("genome" + str(num) + ".txt has been generated")
+print("Genome" + str(num) + ".txt has been generated")
