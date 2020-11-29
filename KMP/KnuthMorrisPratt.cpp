764f3af34b1e9920ebd2bf51af7b5f111e9703ff
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-11-29 01:20:34 +0000

    KMP pseudocode variable refactoring

--- KMP/KnuthMorrisPratt.cpp
+++ KMP/KnuthMorrisPratt.cpp
@@ -2,7 +2,7 @@
 // Created by Isaac on 20/11/2020.
 //
 
-// m = length of pattern; n = length of searchable text; k = size of alphabet
+// m = length of pattern; n = length of searchable text; needPos = size of alphabet
 // Preprocessing time: O(m)
 // Matching time: O(n)
 // Space: O(m)
@@ -11,33 +11,32 @@
 
 #include "KnuthMorrisPratt.h"
 
-std::string KnuthMorrisPratt::StringSearch(std::string needle, std::string haystack) {
+int KnuthMorrisPratt::StringSearch(std::string needle, std::string haystack) {
 	//input:
-	//    an array of characters, S (the text to be searched)
-	//    an array of characters, W (the word sought)
+	//    an array of characters, haystack (the text to be searched)
+	//    an array of characters, needle (the word sought)
 	//output:
-	//    an array of integers, P (positions in S at which W is found)
-	//    an integer, nP (number of positions)
+	//    an array of integers, pos (positions in haystack at which needle is found)
+	//    an integer, numPos (number of positions)
 	//
 	//define variables:
-	//    an integer, j ← 0 (the position of the current character in S)
-	//    an integer, k ← 0 (the position of the current character in W)
-	//    an array of integers, T (the table, computed elsewhere)
+	//    an integer, hayPos = 0 (the position of the current character in haystack)
+	//    an integer, needPos = 0 (the position of the current character in needle)
+	//    an array of integers, table (the table, computed elsewhere)
 	//
-	//let nP ← 0
+	//let numPos = 0
 	//
-	//while j < length(S) do
-	//    if W[k] = S[j] then
-	//        let j ← j + 1
-	//        let k ← k + 1
-	//        if k = length(W) then
-	//            (occurrence found, if only first occurrence is needed, m ← j - k  may be returned here)
-	//            let P[nP] ← j - k, nP ← nP + 1
-	//            let k ← T[k] (T[length(W)] can't be -1)
+	//while hayPos < length(haystack) do
+	//    if needle[needPos] = haystack[hayPos] then
+	//        let hayPos = hayPos + 1
+	//        let needPos = needPos + 1
+	//        if needPos = length(needle) then
+	//            (occurrence found, if only first occurrence is needed, m = hayPos - needPos  may be returned here)
+	//            let pos[numPos] = hayPos - needPos, numPos = numPos + 1
+	//            let needPos = table[needPos] (table[length(needle)] can't be -1)
 	//    else
-	//        let k ← T[k]
-	//        if k < 0 then
-	//            let j ← j + 1
-	//            let k ← k + 1
-	return std::string();
+	//        let needPos = table[needPos]
+	//        if needPos < 0 then
+	//            let hayPos = hayPos + 1
+	//            let needPos = needPos + 1
 }
